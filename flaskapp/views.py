a77ce2fdfd12d40e7e968de5d2dde84abb0fc092
awalesushil <ssushil.awale@gmail.com>
2018-09-30 13:16:41 +0545

    Created Basic Flask template

---
+++ flaskapp/views.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import render_template
+from datetime import datetime
+from . import app
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
